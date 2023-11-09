8aea83aaa4776aaf0d4452e16527516b25deaf00
fjqz177 <fjqz177@139.com>
2023-11-09 17:25:21 +0800

    使用GPT-4 Turbo改进中文注释

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,42 @@
+// 导入tokio相关的异步I/O特性，包括异步读写流和缓冲读取
 use tokio::io::{AsyncBufReadExt, AsyncWriteExt, BufReader};
 use tokio::net::{TcpListener};
 use tokio::sync::broadcast;
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
+// `tokio::main` 宏，表示异步main函数，入口点为异步程序
 #[tokio::main]
 async fn main() {
-    // 创建一个 TCP 监听器，默认端口为 7711
+    // 绑定TCP监听器到本地地址和端口7711
     let listener = TcpListener::bind("127.0.0.1:7711").await.unwrap();
 
-    // 创建一个 broadcast channel，来向所有客户端发送消息
-    // 我们使用 (usize, String) 元组，usize 表示排除特定的发送者的 ID，String 是消息内容
+    // 创建一个广播通道，可以向订阅者广播消息
     let (tx, _rx) = broadcast::channel(10);
 
-    // 使用 Arc 和 RwLock 来共享和更新客户端信息
-    // 这里存储每个客户端的昵称，并与其未来（futures）关联
+    // 用来共享和更新客户端信息的线程安全的HashMap
     let clients = Arc::new(RwLock::new(HashMap::<usize, String>::new()));
 
-    // 初始化客户端 ID。实际上，这种递增 ID 分配方式在生产应用中不是很好的方式，因为它很容易预测。
-    // 在实际应用中，你会想要使用更安全的 ID 生成机制。
+    // 用来给客户端分配唯一ID的计数器，此方法在实际生产中不推荐
     let mut next_id = 0;
 
     println!("Server running on port 7711");
 
     loop {
-        // 接受新的连接
+        // 接收新的TCP连接
         let (mut socket, addr) = listener.accept().await.unwrap();
 
         println!("New connection: {}", addr);
 
+        // 克隆一份发送端，供新的客户端使用
         let tx = tx.clone();
+        // 为新的客户端订阅接收通道
         let mut rx = tx.subscribe();
 
+        // 克隆clients的Arc以在异步块中使用
         let clients = Arc::clone(&clients);
 
-        // 分配新的客户端 ID
+        // 为每个新的连接创建唯一的客户端ID 
         let client_id = {
             let mut clients = clients.write().unwrap();
             next_id += 1;
@@ -42,48 +44,52 @@ async fn main() {
             next_id
         };
 
+        // 对每个连接单独启动一个异步任务
         tokio::spawn(async move {
-            // 分读写两部分操作
+            // 分隔TCP流为读取器和写入器
             let (reader, mut writer) = socket.split();
 
-            // 创建带缓冲的读取器
+            // 对读取器部分进行缓冲
             let mut reader = BufReader::new(reader);
             let mut line = String::new();
 
-            // 发送欢迎消息
+            // 向新客户端发送欢迎消息
             let welcome_message = format!("Welcome! Your ID is {}. Use '/nick NAME' to set a nickname.\n", client_id);
             writer.write_all(welcome_message.as_bytes()).await.unwrap();
 
+            // 启动一个事件循环，直到客户端断开连接
             loop {
                 tokio::select! {
-                    // 读取客户端发来的消息
+                    // 读取客户端信息
                     read_result = reader.read_line(&mut line) => {
                         match read_result {
                             Ok(0) => break, // 客户端断开连接
                             Ok(_) => {
-                                // 处理客户端发来的消息
+                                // 处理客户端发来的信息
                                 handle_client_message(client_id, &line, &clients, tx.clone()).await;
-                                // 清空收到的消息以便下一次循环读取
+                                // 重置line，为下一条信息做准备
                                 line.clear();
                             },
                             Err(e) => {
+                                // 出现读取错误
                                 eprintln!("An error occurred while reading from socket: {:?}", e);
                                 break;
                             },
                         }
                     },
 
-                    // 接受来自其他客户端的消息，并发送
+                    // 接收其他客户端发来的信息
                     recv_result = rx.recv() => {
                         match recv_result {
                             Ok((sender_id, msg)) => {
+                                // 仅转发不是由自己发送的信息
                                 if sender_id != client_id {
-                                    // 客户端不是消息的原始发送者，将其转发出去
                                     writer.write_all(msg.as_bytes()).await.expect("Write to socket failed.");
                                     writer.flush().await.expect("Flush to socket failed.");
                                 }
                             },
                             Err(e) => {
+                                // 接收错误
                                 eprintln!("An error occurred while receiving: {:?}", e);
                                 break;
                             },
@@ -92,9 +98,10 @@ async fn main() {
                 }
             }
 
+            // 打印断开连接的客户端信息
             println!("Client {} disconnected", client_id);
             {
-                // 客户端断开连接，移除其在 HashMap 中的条目
+                // 从HashMap中移除断开连接的客户端
                 let mut clients = clients.write().unwrap();
                 clients.remove(&client_id);
             }
@@ -102,7 +109,7 @@ async fn main() {
     }
 }
 
-// 处理客户端消息，并在必要时更新昵称
+// 用于处理发送者的消息，可能会更新昵称，或者广播消息给其他客户端
 async fn handle_client_message(
     client_id: usize,
     line: &str,
@@ -110,15 +117,15 @@ async fn handle_client_message(
     tx: broadcast::Sender<(usize, String)>,
 ) {
     if line.starts_with("/nick ") {
-        // 设置或更新客户端的昵称
-        let nick = line[6..].trim(); // 假定命令格式总是正确的
+        // 用户尝试设置新的昵称
+        let nick = line[6..].trim(); // 提取新昵称，假设命令格式总是正确的
         let mut clients = clients.write().unwrap();
-        clients.insert(client_id, nick.to_string());
+        clients.insert(client_id, nick.to_string()); // 更新HashMap中的昵称
     } else {
-        // 否则就转发消息到其他客户端
+        // 其他信息将被广播给所有客户端
         let clients = clients.read().unwrap();
         let nick = clients.get(&client_id).unwrap();
-        let msg = format!("{}> {}", nick, line);
-        tx.send((client_id, msg)).unwrap();
+        let msg = format!("{}> {}", nick, line); // 格式化消息
+        tx.send((client_id, msg)).unwrap(); // 发送消息
     }
 }
